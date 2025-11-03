e6a84bfb7966bba9f692e5d1eed4a3a4bddf5702
Christopher <gomeschristopher@hotmail.com>
2025-11-03 13:59:43 -0300

    add rabbitmq

--- src/main/java/com/gomeschristopher/payments/controller/PaymentController.java
+++ src/main/java/com/gomeschristopher/payments/controller/PaymentController.java
@@ -3,6 +3,8 @@ package com.gomeschristopher.payments.controller;
 import com.gomeschristopher.payments.dto.PaymentDTO;
 import com.gomeschristopher.payments.service.PaymentService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,9 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @GetMapping
     public Page<PaymentDTO> list(@PageableDefault Pageable pageable) {
         return service.getAll(pageable);
@@ -30,6 +35,11 @@ public class PaymentController {
     @PostMapping
     public ResponseEntity<PaymentDTO> create(@RequestBody PaymentDTO dto) {
         PaymentDTO createdPayment = service.create(dto);
+
+
+        //Message message = new Message(("Payment created with ID: " + createdPayment.id()).getBytes());
+        rabbitTemplate.convertAndSend("pagamentos.ex", "", createdPayment);
+
         return ResponseEntity.ok(createdPayment);
     }
 
